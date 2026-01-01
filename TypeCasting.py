e21079a7a259e0e56dfead1f64fb2eaac233d273
AhamedHaseen <your_username@users.noreply.github.com>
2026-01-01 14:14:53 +0530

    add TypeCasting.py with variable definitions and type checks

---
+++ TypeCasting.py
@@ -0,0 +1,20 @@
+
+name = "Ahamed"
+age = 22
+height = 5.9
+is_student = True
+
+print(type(name))      # Output: <class 'str'>
+print(type(age))       # Output: <class 'int'>  
+print(type(height))    # Output: <class 'float'>
+print(type(is_student))# Output: <class 'bool'>
+
+print()
+
+age = float(age)
+height = int(height)
+is_student = str(is_student)
+
+print(type(age))       # Output: <class 'float'>
+print(type(height))    # Output: <class 'int'>  
+print(type(is_student))# Output: <class 'str'>
