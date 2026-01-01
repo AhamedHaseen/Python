a5c281201a8c6ed99ac33cfdb3cb635b539f6739
AhamedHaseen <your_username@users.noreply.github.com>
2026-01-01 14:57:16 +0530

    refactor input.py to comment out name and age prompts; add rectangle area calculation

--- input.py
+++ input.py
@@ -1,4 +1,4 @@
-name = input("Enter your name: ")
+'''name = input("Enter your name: ")
 age = int(input("How old are you? "))
 
 #age = int(age) 
@@ -6,4 +6,11 @@ age += 1
 
 print(f"My Name is {name}")
 print("Happy Birthday")
-print(f"I am {age} years old.")
+print(f"I am {age} years old.")'''
+
+#Exercise = Rectangle Area Calculation
+length = float(input("Enter Length of Rectangle: "))
+width = float(input("Enter Width of Rectangle: "))
+area = length * width
+
+print(f"The area of the rectangle is {area}")
