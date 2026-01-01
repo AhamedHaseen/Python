9e10a099bcd529fc9b0e6bf7a5dd22b8005c1fa2
AhamedHaseen <your_username@users.noreply.github.com>
2026-01-01 13:43:35 +0530

    add variables to store personal information and print details

---
+++ variables.py
@@ -0,0 +1,12 @@
+first_name = "Ahamed"
+Last_name = "Haseen"
+age = 22
+is_Student = True
+height = 5.7
+
+name = first_name + " " + Last_name
+
+print("My Name is : ", name)
+print("My Age is: ", age)
+print("Am I a Student?: ", is_Student)
+print("My Height is: ", height)
