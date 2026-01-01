0ef4d805a1c0eb02b0516092c5108a65bbc9cce6
AhamedHaseen <your_username@users.noreply.github.com>
2026-01-01 14:22:42 +0530

    add boolean type casting for name and name_2 variables

--- TypeCasting.py
+++ TypeCasting.py
@@ -1,6 +1,7 @@
 
 #name = int("Ahamed") # This will raise a ValueError
 name = "Ahamed"
+name_2 = ""
 age = 22
 height = 5.9
 is_student = True
@@ -15,12 +16,18 @@ print()
 age = float(age)
 height = int(height)
 is_student = str(is_student)
+name = bool(name)
+name_2 = bool(name_2)
 
 print(type(age))       # Output: <class 'float'>
 print(type(height))    # Output: <class 'int'>  
 print(type(is_student))# Output: <class 'str'>
+print(type(name))      # Output: <class 'bool'>
+print(type(name_2))    # Output: <class 'bool'>
 
 print()
 print(age)             # Output: 22.0
 print(height)          # Output: 5
 print(is_student)      # Output: True
+print(name)            # Output: True
+print(name_2)          # Output: False
