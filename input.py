1d682603062d35ea00568c0eeba21a1ab71de9d4
AhamedHaseen <your_username@users.noreply.github.com>
2026-01-01 15:06:57 +0530

    refactor input.py to uncomment rectangle area output and add a new exercise section

--- input.py
+++ input.py
@@ -8,6 +8,7 @@ print(f"My Name is {name}")
 print("Happy Birthday")
 print(f"I am {age} years old.")'''
 
+'''
 #Exercise = Rectangle Area Calculation
 length = float(input("Enter Length of Rectangle: "))
 width = float(input("Enter Width of Rectangle: "))
@@ -16,3 +17,5 @@ area = length * width
 #print(f"The area of the rectangle is {area}")
 #number lock + alt + 0178 = ²
 print(f"The area is {area} cm²")
+'''
+#Exercise = 
