ac86d3b36946e3ba8defc0ffeb2d1529cc65df19
AhamedHaseen <your_username@users.noreply.github.com>
2026-01-01 14:53:14 +0530

    add input prompts for name and age in input.py

---
+++ input.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+age = int(input("How old are you? "))
+
+#age = int(age) 
+age += 1
+
+print(f"My Name is {name}")
+print("Happy Birthday")
+print(f"I am {age} years old.")
