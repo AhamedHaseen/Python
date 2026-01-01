4044b85f81b18919b4cab46baac3d1a924590977
AhamedHaseen <your_username@users.noreply.github.com>
2026-01-01 15:00:41 +0530

    refactor input.py to update rectangle area output format and comment out previous area print statement

--- input.py
+++ input.py
@@ -13,4 +13,6 @@ length = float(input("Enter Length of Rectangle: "))
 width = float(input("Enter Width of Rectangle: "))
 area = length * width
 
-print(f"The area of the rectangle is {area}")
+#print(f"The area of the rectangle is {area}")
+#number lock + alt + 0178 = ²
+print(f"The area is {area} cm²")
