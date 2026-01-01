a06bb6c8aac20744e4c54860759cb944e7fe09a2
AhamedHaseen <your_username@users.noreply.github.com>
2026-01-01 13:57:08 +0530

    add email variable and print statements for personal details

--- variables.py
+++ variables.py
@@ -3,6 +3,7 @@ Last_name = "Haseen"
 age = 22 #integer variable
 is_Student = True #boolean variable
 height = 5.7 #float variable
+email = "ahamed.haseen@example.com" #string variable
 
 name = first_name + " " + Last_name
 
@@ -10,9 +11,24 @@ print("My Name is : ", name)
 print("My Age is: ", age)
 print("Am I a Student?: ", is_Student)
 print("My Height is: ", height)
+print("My Email is ", email)
 
 print()
 
 #Formatted string
 
 print(f"Hello {name}")
+print(f"My Age is {age}")
+print(f"Am I Student? : {is_Student}")
+print(f"My Height Is {height}")
+print(f"My Email is {email}")
+
+
+print()
+
+is_Citizen = False
+
+if is_Citizen:
+    print(f"Ahamed Haseen is a {is_Citizen}")
+else:
+    print(f"{name} is not a Citizen")
