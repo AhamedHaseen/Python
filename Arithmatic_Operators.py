6595ec43a020ae7d7b271e6989d7e4ce5e72e1db
AhamedHaseen <your_username@users.noreply.github.com>
2026-01-02 07:51:12 +0530

    add Arithmatic_Operators.py with basic arithmetic operations and exponentiation example

---
+++ Arithmatic_Operators.py
@@ -0,0 +1,21 @@
+
+a = 10
+
+#a += 5 #addition
+#a -= 3 #subtraction
+#a *= 2 #multiplication
+#a /= 4 #division
+#a %= 3 #remainder / moduls
+#a **= 2 #exponation
+#a //= 4 #floor division
+
+#Math Operations
+x = 3.14
+y = -4
+z = 5
+
+#result = round(x) #rounding off
+#result = abs(y) #absolute value
+result = pow(z, 3) #exponentiation
+
+print(result)
