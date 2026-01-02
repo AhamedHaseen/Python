502baf4278d71f33c9090d531748ba1dfd17b54a
AhamedHaseen <your_username@users.noreply.github.com>
2026-01-02 07:35:19 +0530

    fix typo in variable name and improve output formatting in MadlibsGame.py

--- MadlibsGame.py
+++ MadlibsGame.py
@@ -2,11 +2,11 @@
 adjective1 = input("Enter an adjective (description): ")
 noun1 = input("Enter a noun (person, place, or thing): ")
 adjective2 = input("Enter an adjective (description): ")
-verp1 = input("Enter a verb ending with 'ing': ")
+verb1 = input("Enter a verb ending with 'ing': ")
 adjective3 = input("Enter an adjective (description): ")
 
 
-print(f"Today i went to a {adjective1} Zoo")
+print(f"Today i went to a {adjective1} Zoo.")
 print(f"In an exhibit, I saw a {noun1}")
 print(f"{noun1} was {adjective2} and {verb1}")
-print(f"I was {adjective3}")
+print(f"I was {adjective3}!")
